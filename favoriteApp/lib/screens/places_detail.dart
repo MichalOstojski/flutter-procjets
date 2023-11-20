fe855a5bc7e5fd410df04677c8d88cd7be03e014
MichalOstojski <73385013+MichalOstojski@users.noreply.github.com>
2023-11-20 15:29:30 +0100

    Add files via upload

---
+++ favoriteApp/lib/screens/places_detail.dart
@@ -0,0 +1,81 @@
+import 'package:favorite_app/models/place.dart';
+import 'package:favorite_app/screens/map.dart';
+import 'package:flutter/material.dart';
+
+class PlaceDatailScreen extends StatelessWidget {
+  const PlaceDatailScreen({super.key, required this.place});
+  final Place place;
+
+  String get locationImage {
+    final lat = place.location.latitude;
+    final lng = place.location.longtitude;
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng&zoom=16&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:M%7C$lat,$lng&key=AIzaSyC32DlblhAM2uK4zYifqJWDGxO7STaJHKI';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(place.title),
+      ),
+      body: Stack(
+        children: [
+          Image.file(
+            place.image,
+            fit: BoxFit.cover,
+            height: double.infinity,
+            width: double.infinity,
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Column(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (ctx) => MapScreen(
+                          location: place.location,
+                          isSelecting: false,
+                        ),
+                      ),
+                    );
+                  },
+                  child: CircleAvatar(
+                    radius: 70,
+                    backgroundImage: NetworkImage(locationImage),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 25,
+                    vertical: 15,
+                  ),
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.transparent,
+                        Colors.black54,
+                      ],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
+                  ),
+                  child: Text(
+                    place.location.address,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                        color: Theme.of(context).colorScheme.onBackground),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
