75b6ce031e38db1ef35e9945cfa0a5315d04620e
MichalOstojski <73385013+MichalOstojski@users.noreply.github.com>
2023-11-20 14:57:09 +0100

    Add files via upload

---
+++ expenseTrackerApp/lib/models/expense.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
+import 'package:intl/intl.dart';
+
+final formatter = DateFormat.yMd();
+
+const uuid = Uuid();
+
+enum Category { food, travel, relax, work, value }
+
+const categoryIcons = {
+  Category.food: Icons.add_shopping_cart_sharp,
+  Category.travel: Icons.airplanemode_active_rounded,
+  Category.relax: Icons.beach_access_outlined,
+  Category.work: Icons.business_center_rounded,
+};
+
+class Expense {
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.date,
+    required this.category,
+  }) : id = uuid.v4();
+
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+  final Category category;
+
+  String get formattedDate {
+    return formatter.format(date);
+  }
+}
+
+class ExpenseBucket {
+  const ExpenseBucket({
+    required this.category,
+    required this.expenses,
+  });
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
+
+  final Category category;
+  final List<Expense> expenses;
+  double get totalExpenses {
+    double sum = 0;
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+    return sum;
+  }
+}
