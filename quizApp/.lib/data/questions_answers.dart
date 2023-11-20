806f3a8059ab6e7203915db3ec06b8a1da164fc6
MichalOstojski <73385013+MichalOstojski@users.noreply.github.com>
2023-11-20 14:06:14 +0100

    Add files via upload

---
+++ quizApp/.lib/data/questions_answers.dart
@@ -0,0 +1,58 @@
+import 'package:quiz_app/models/questions.dart';
+
+const questions = [
+  QuizQuestions(
+    'Jak Ostoj ma na imie?',
+    [
+      'Michal',
+      'Kuba',
+      'Wojtek',
+      'Milosz',
+    ],
+  ),
+  QuizQuestions(
+    'Jakie jest ulubione piwo Ostoja?',
+    [
+      'Zimne',
+      'Mocne',
+      'Rozgazowane',
+      'Wylane',
+    ],
+  ),
+  QuizQuestions(
+    'Ulubiony kolor Ostoja?',
+    [
+      'Czerwony',
+      'Niebieski',
+      'Bialy',
+      'Zielony',
+    ],
+  ),
+  QuizQuestions(
+    'Ulubione zwierze Ostoja?',
+    [
+      'Gibon',
+      'Owca',
+      'Tygrys',
+      'Orka',
+    ],
+  ),
+  QuizQuestions(
+    'Jakie jest ulubione warzywo Ostoja',
+    [
+      'Ogorek',
+      'Cukinia',
+      'Papryka',
+      'Pomidor',
+    ],
+  ),
+  QuizQuestions(
+    'Jaka jest ulubiona pizza Ostoja',
+    [
+      'Pepperoni',
+      'Cztery sery',
+      'Caproczosa',
+      'Calzone z nutella',
+    ],
+  ),
+];
